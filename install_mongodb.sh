20f6219b4738e8bcb67f1719fbb96c9dd4aaefb8
naselin <nikita.selin@gmail.com>
2022-10-06 21:44:24 +0300

    HW-04. Trying to fix failed test.

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -2,6 +2,7 @@
 echo "Installing and starting MongoDB..."
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates
 sudo apt-get install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
