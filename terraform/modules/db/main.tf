b365da798e0c02f708c33b09ac35eb11bd65fca4
naselin <nikita.selin@gmail.com>
2022-10-22 11:01:13 +0300

    HW-07 (lesson-09). Terraform modules.

---
+++ terraform/modules/db/main.tf
@@ -0,0 +1,29 @@
+resource "yandex_compute_instance" "db" {
+  name     = "reddit-db"
+  hostname = "reddit-db.internal"
+
+  labels = {
+    tags = "reddit-db"
+  }
+
+  resources {
+    cores         = 2
+    core_fraction = 20
+    memory        = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = var.db_disk_image
+    }
+  }
+
+  network_interface {
+    subnet_id = var.subnet_id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+}
