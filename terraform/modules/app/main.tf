85b5bcf06d19abc77a9a0e84b2f37e015f9d695f
naselin <nikita.selin@gmail.com>
2022-10-23 11:39:25 +0300

    HW-07 (lesson-09). Auto deploy.

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -26,4 +26,22 @@ resource "yandex_compute_instance" "app" {
   metadata = {
     ssh-keys = "ubuntu:${file(var.public_key_path)}"
   }
+
+  connection {
+    type  = "ssh"
+    host  = self.network_interface.0.nat_ip_address
+    user  = "ubuntu"
+    agent = false
+    # путь до приватного ключа
+    private_key = file(var.private_key_path)
+  }
+
+  provisioner "file" {
+    content     = templatefile("${path.module}/files/puma.service.tftpl", { app_database_ip = var.app_database_ip })
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "${path.module}/files/deploy.sh"
+  }
 }
--- terraform/modules/app/variables.tf
+++ terraform/modules/app/variables.tf
@@ -1,10 +1,16 @@
 variable "public_key_path" {
   description = "Path to the public key used for ssh access"
 }
+variable "private_key_path" {
+  description = "Path to the private key used for ssh access"
+}
 variable "app_disk_image" {
   description = "Disk image for reddit app"
   default     = "reddit-app-base"
 }
 variable "subnet_id" {
   description = "Subnets for modules"
+}
+variable "app_database_ip" {
+  description = "IP address of the database server"
 }
